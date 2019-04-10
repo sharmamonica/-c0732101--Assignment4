1d92656e65e5bf3db534212befa9ee875cc8b4c7
visualstudio <visualstudio@122-PC10>
2019-04-10 09:53:34 -0400

    Sec A

--- c0732101-Assignment4/Program.cs
+++ c0732101-Assignment4/Program.cs
@@ -3,13 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
-namespace c0732101_Assignment4
+
+
+namespace C0732101_a4
 {
     class Program
     {
+        ArrayList Beowulf;
+        readonly int linecount = 0;
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+            p.Run();
+
         }
+
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+        public void ReadTextFiles()
+        {
+            using (StreamReader file = new StreamReader("U:/Users/732101/Beowulf.txt"))
+            {
+
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+
+                }
+                file.Close();
+                Console.WriteLine($"File has{counter} lines");
+
+
+
+
+
+            }
+
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+
+            }
+            return countSpaces;
+        }
+
+
+
+
+
+
     }
 }
